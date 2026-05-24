804900309b4d5e91001d8744c56c759ead859674
Promethyxx <peter.mboyo@ik.me>
2026-05-24 19:01:58 +0200

    build.rs

--- build.rs
+++ build.rs
@@ -3,16 +3,18 @@ fn main() {
     windows_resources();
 }
 
+#[cfg(target_os = "windows")]
 fn windows_resources() {
-    // On vérifie si la cible finale (target) est bien Windows
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
-    
-    if target_os == "windows" {
-        let icon = std::path::Path::new("assets/Oxycash_icon.ico");
-        if icon.exists() {
-            let mut res = winresource::WindowsResource::new();
-            res.set_icon(icon.to_str().unwrap());
-            res.compile().expect("Failed to compile Windows resources");
-        }
+    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() != Ok("windows") {
+        return;
+    }
+    let icon = std::path::Path::new("assets/Oxycash_icon.ico");
+    if icon.exists() {
+        let mut res = winresource::WindowsResource::new();
+        res.set_icon(icon.to_str().unwrap());
+        res.compile().expect("Failed to compile Windows resources");
     }
 }
+
+#[cfg(not(target_os = "windows"))]
+fn windows_resources() {}
