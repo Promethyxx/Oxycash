74c8e160fb407fc591fd4b9f79f36884af20b702
Promethyxx <peter.mboyo@ik.me>
2026-05-30 15:35:55 +0200

    CI make me cry

--- build.rs
+++ build.rs
@@ -8,8 +8,8 @@ fn windows_resources() {
     if std::env::var("CARGO_CFG_TARGET_OS").as_deref() != Ok("windows") {
         return;
     }
-    println!("cargo:rustc-link-arg=/SUBSYSTEM:WINDOWS");
-    println!("cargo:rustc-link-arg=/ENTRY:mainCRTStartup");
+    println!("cargo:rustc-link-arg-bins=/SUBSYSTEM:WINDOWS");
+    println!("cargo:rustc-link-arg-bins=/ENTRY:mainCRTStartup");
     let icon = std::path::Path::new("assets/Oxycash_icon.ico");
     if icon.exists() {
         let mut res = winresource::WindowsResource::new();
